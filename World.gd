2d44552948d9583159365563c0d2c464bc69a55e
tomnijhof <tom.nijhof@cytosmart.com>
2023-02-06 22:21:26 +0100

    Connect with online backend
    Signed-off-by: tomnijhof <tom.nijhof@cytosmart.com>

--- World.gd
+++ World.gd
@@ -15,7 +15,7 @@ func fetch_new_levels(_new_level_number):
 	var fields = {"id": _new_level_number}
 	var query_string = to_json(fields)
 	var headers = ["Content-Type: application/x-www-form-urlencoded", "Content-Length: " + str(query_string.length())]
-	var result = $HTTPRequest.request("http://127.0.0.1:8000/get_level_by_number", 
+	var result = $HTTPRequest.request("https://platformer.deta.dev/get_level_by_number", 
 		PoolStringArray( ),
 		true, 
 		HTTPClient.METHOD_POST, 
@@ -23,9 +23,11 @@ func fetch_new_levels(_new_level_number):
 	
 
 func _on_request_completed(result, response_code, headers, body):
-	var json = JSON.parse(body.get_string_from_utf8())
+	var body_content = body.get_string_from_utf8()
+	print(body_content)
+	var json = JSON.parse(body_content)
 	print(json.result)
-	doorMapping = json.result["doorMapping"]
+	doorMapping = json.result
 	httpActive = false
 
 func _physics_process(_delta):
@@ -36,18 +38,11 @@ func _physics_process(_delta):
 func enter_door(door):
 	player.last_door = door
 	var door_name = door.name
-	var encoded_level = doorMapping[door_name]["level_encoded"]
-	var new_level_number = doorMapping[door_name]["level_num"]
+	var encoded_level = doorMapping[door_name]["encode_level"]
+	var new_level_number = doorMapping[door_name]["number"]
 	tileMap.apply_level(encoded_level)
 	_update_level(new_level_number)
 
 func _update_level(new_level_number: int):
 	var new_level = fetch_new_levels(new_level_number)
 	player.reset_position()
-	
-func _input(ev):
-	if ev is InputEventKey and ev.scancode == KEY_1:
-		tileMap.apply_level("9CCa0553bcd+efAA8")
-	
-	if ev is InputEventKey and ev.scancode == KEY_2:
-		tileMap.apply_level("8A9AAAA+")
